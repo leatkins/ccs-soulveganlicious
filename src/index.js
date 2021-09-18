89c752e500101959f443e2288492ba4e1c8eac70
LeRon Atkins <leatkins@aboveall-media.tech>
2021-09-18 12:31:33 -0500

    Added cover imagne and modified css

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import CoverLogo from './images/CoverLogo2.png';
 
   ReactDOM.render(
     <React.StrictMode>
-      <div className="container-fluid">
+      <div className="cover-container">
       <div className="coverImage">
           <div className="logo text-center">
               <img src={CoverLogo} width="500px" alt="CCs Vegan Spot Logo" />
               <br />
-              <a href="https://www.facebook.com/ccsveganspot" target="_blank" rel="noreferrer"><button className="btn btn-secondary"><h5>Like us on Facebook<br /> @ccsveganspot</h5></button></a>
+              <a href="https://www.facebook.com/ccsveganspot" className="noDecoration" target="_blank" rel="noreferrer"> ::: <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+</svg> :::  LIKE US ON FACEBOOK</a>
             </div>
             <div className="text-center beginBtn">
                 <button className="btn-lg btn-warning" onClick={OpenApp}><h1>START ORDER</h1></button>
